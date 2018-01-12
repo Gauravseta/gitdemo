9326d99a91f0857376fcd1e9160d187ecd5509a7
gauravseta@gmail.com <gauravseta@gmail.com>
2018-01-12 21:53:50 +0530

    one more space

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app=express();
 var path=require('path');
 
 
-app.use( express.static(path.join(__dirname)));
+app.use(  express.static(path.join(__dirname)));
 
 
 app.get('/',function(req,res){
