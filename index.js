a05138d1b11a7530aaf41a71e87bbc2e440366a4
gauravseta@gmail.com <gauravseta@gmail.com>
2018-01-12 21:55:51 +0530

    Revert "it is second commit"
    This reverts commit 4285446ec8ecafaf06f49d9f6cf887073a1dcac5.

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var app=express();
 var path=require('path');
 
 
-app.use(   express.static(path.join(__dirname)));
+app.use(  express.static(path.join(__dirname)));
 
 
-app.get('/',function(req,res){
+app.get(' /',function(req,res){
 
 	res.sendFile(path.join(__dirname+'/index.html'));
 
