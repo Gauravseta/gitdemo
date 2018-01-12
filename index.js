11854ab6bb35beb60d6059741f78de161a365001
gauravseta@gmail.com <gauravseta@gmail.com>
2018-01-12 22:11:57 +0530

    making more space

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var path=require('path');
 app.use(  express.static(path.join(__dirname)));
 
 
-app.get(' /',function(req,res){
+app.get(' / ',function(req,res){
 
 	res.sendFile(path.join(__dirname+'/index.html'));
 
