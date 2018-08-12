8491d0596c37fb26bb00b71177360a6d55418b7a
gauravseta <gauravseta@gmail.com>
2018-08-12 13:00:03 +0530

    removing 11

--- folder1/index1.js
+++ folder1/index1.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname)));
 
 app.get('/',function(req,res){
 
-	res.sendFile(path.join(__dirname+'/index.html11'));
+	res.sendFile(path.join(__dirname+'/index.html'));
 
 });
 app.listen(process.env.PORT||8080);
